92fc341a86770fc7a0a0ea6cb5ee294a87925784
Diego Griep <fale@diegogriep.com.br>
2025-12-06 16:42:38 +0000

    feat: setup jest and unit test

---
+++ jest.config.js
@@ -0,0 +1,33 @@
+const { createDefaultPreset } = require('ts-jest')
+
+const tsJestTransformCfg = createDefaultPreset().transform
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  // Automatically clear mock calls, instances, contexts and results before every test
+  clearMocks: true,
+
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'v8',
+
+  // The test environment that will be used for testing
+  testEnvironment: 'jsdom',
+  globals: {
+    globals: {
+      'ts-jest': {
+        tsConfig: {
+          importHelpers: true
+        }
+      }
+    }
+  },
+  transform: {
+    ...tsJestTransformCfg
+  }
+}
